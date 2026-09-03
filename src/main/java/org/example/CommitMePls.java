2185b376794d7f3686dd5cfb0307f23cbbf588ff
akravchuk <al.kravchuk@kpedev.by>
2026-09-03 12:59:03 +0300

    не придумал название коммита

--- src/main/java/org/example/CommitMePls.java
+++ src/main/java/org/example/CommitMePls.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class CommitMePls {
+
+    private String message;
+
+    public CommitMePls(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
