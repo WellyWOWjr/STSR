65fc907b5647a560c290570c0d573b8c8663c49f
akravchuk <al.kravchuk@kpedev.by>
2026-09-03 12:56:54 +0300

    print all nums more than 30 from random list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        List<Integer> list = IntStream.range(0, 10)
+                .map(i -> new Random().nextInt(100))
+                .boxed()
+                .toList();
+
+        list.stream()
+                .filter(n -> n > 30)
+                .forEach(System.out::println);
     }
 }
